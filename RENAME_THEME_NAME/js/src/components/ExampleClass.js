1fe250a1913f6a0da373ccb75b47137ee131c2ab
kaganov5 <kalaganov5@gmail.com>
2024-11-26 18:51:14 +0300

    upd js

---
+++ RENAME_THEME_NAME/js/src/components/ExampleClass.js
@@ -0,0 +1,6 @@
+
+export class ExampleClass {
+  constructor () {
+    console.log('hello')
+  }
+}
--- RENAME_THEME_NAME/js/src/main.js
+++ RENAME_THEME_NAME/js/src/main.js
@@ -1,13 +1,6 @@
-import { LangSwitcher } from './components/LangSwitcher';
-import { SliderPresentation } from './components/SliderPresentation';
-import { WidgetQuiz } from './components/WidgetQuiz';
-import { Accordion } from './components/Accordion';
-import { MenuToggle } from './components/MenuToggle';
+import { ExampleClass } from './components/ExampleClass';
 
 document.addEventListener('DOMContentLoaded', function () {
-  new LangSwitcher('[data-ui-toggle="lang-dropdown"]', 'lang-dropdown');
-  new SliderPresentation().init();
-  new WidgetQuiz().init();
-  new Accordion('.accordion');
-  new MenuToggle();
+  new ExampleClass();
 });
+
